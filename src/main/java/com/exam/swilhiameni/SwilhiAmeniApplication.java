9614278962e5307ba5630b683a1bf2965e61115e
swilhi <ameni.swilhi@esprit.tn>
2023-05-06 02:50:35 +0100

    first commit

---
+++ src/main/java/com/exam/swilhiameni/SwilhiAmeniApplication.java
@@ -0,0 +1,13 @@
+package com.exam.swilhiameni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwilhiAmeniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwilhiAmeniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/swilhiameni/SwilhiAmeniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.swilhiameni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwilhiAmeniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
